a293ba886073529b2400c81e02d4943cb84ef74d
Paul Son <bigbeerhug@gmail.com>
2025-08-01 00:10:37 -0400

    Initial commit

---
+++ UrbanLeague/UrbanLeagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanLeagueApp.swift
+//  UrbanLeague
+//
+//  Created by Paul Son on 8/1/25.
+//
+
+import SwiftUI
+
+@main
+struct UrbanLeagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UrbanLeagueTests/UrbanLeagueTests.swift
@@ -0,0 +1,17 @@
+//
+//  UrbanLeagueTests.swift
+//  UrbanLeagueTests
+//
+//  Created by Paul Son on 8/1/25.
+//
+
+import Testing
+@testable import UrbanLeague
+
+struct UrbanLeagueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
